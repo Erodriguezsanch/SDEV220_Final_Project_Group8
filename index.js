79131cc2f7a1ded965d30f8b3d0dab40682df337
Eli Walters <ewalters37@ivytech.edu>
2024-11-24 16:53:56 -0500

    Update Project Files

---
+++ index.js
@@ -0,0 +1,16 @@
+// Package Imports
+const express = require("express");
+
+// Express
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Server listening on port 3000!");
+});
